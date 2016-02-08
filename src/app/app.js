77b31aa22f638074967cfc9b1f364c8a78113670
Júlio L. Jr <julio.leitao.jr@gmail.com>
2016-02-08 13:50:43 -0300

    adicionando parsley na validacao de criar usuario

--- src/app/app.js
+++ src/app/app.js
@@ -9,4 +9,7 @@
 	app.config(function(blockUIConfig) {
 		blockUIConfig.delay = 100;
 	});
+	// criar Jenkins na Openshift
+	// usar o HEROKU mesmo :D
+	// ApiEndPoint usando NODE_ENV para determinar se aponta para o remoto
 })();
