64c3de2fe7088a6483a226c87dd20e20314a02fb
Júlio L. Jr <julio.leitao.jr@gmail.com>
2016-02-10 17:31:16 -0300

    alterando formato para deploy

--- src/app/app.js
+++ src/app/app.js
@@ -7,7 +7,7 @@
 		'blockUI'
 	]);
 	
-	app.constant('ApiUrl', { url: 'http://localhost:8080/api' });
+	app.constant('ApiUrl', { url: 'http://sistemauto.herokuapp.com/api' });
 
 	app.config(function(blockUIConfig) {
 		blockUIConfig.delay = 100;
