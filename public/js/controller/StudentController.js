08f4ad0414cd25255904670ff7280ef2df15e595
Júlio L. Jr <julio.leitao.jr@gmail.com>
2016-03-03 09:54:34 -0300

    adicionando modal de cadastro via fab

--- public/js/controller/StudentController.js
+++ public/js/controller/StudentController.js
@@ -7,10 +7,23 @@
      */
     angular.module('app').controller('StudentController', [
         '$scope',
+        '$mdDialog',
         StudentController
     ]);
 
-    function StudentController($scope) {
+    function StudentController($scope, $mdDialog) {
+        $scope.showForm = function() {
+            $mdDialog.show({
+                controller: novoAlunoController,
+                templateUrl: '../view/newStudent.html',
+                parent: angular.element(document.body)
+            });
+        };
 
+        /**
+         * Controller para o modal de cadastro de aluno
+         */
+        function novoAlunoController($scope) {
+        };
     }
 })();
