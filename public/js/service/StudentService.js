ce62f337e41daa3b59e75b6b31807061f9bc8723
Júlio L. Jr <julio.leitao.jr@gmail.com>
2016-03-03 12:34:16 -0300

    adicionando recurso de student e service'

--- public/js/service/StudentService.js
+++ public/js/service/StudentService.js
@@ -14,5 +14,57 @@
     function StudentService($http, $q, ApiUrl) {
         var self = this;
 
+        var cache = {};
+
+        this.getStudents = function() {
+            return $http.get(ApiUrl.url + '/student');
+        };
+
+        this.getStudent = function(id) {
+            var deferred = $q.defer();
+            if (self.cache.id !== undefined) {
+                deferred.resolve({
+                    data: cache.id
+                });
+            } else {
+                $http.get(ApiUrl.url + '/student/' + id).then(function(info) {
+                    cache.id = info.data;
+                    deferred.resolve(info);
+                }, function(error) {
+                    deferred.reject(error);
+                });
+            }
+            return deferred.promise;
+        };
+
+        this.postStudent = function() {
+            var deferred = $q.defer();
+
+            $http.post(ApiUrl.url + '/student', data).then(function(info) {
+                cache._id = info.data;
+                deferred.resolve(info);
+            }, function(error) {
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        };
+
+        this.updateStudent = function(data) {
+            var deferred = $q.defer();
+
+            $http.put(ApiUrl.url + '/student/' + data._id, data).then(function(info) {
+                cache._id = info.data;
+                deferred.resolve(info);
+            }, function(error) {
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
+        };
+
+        this.removeStudent = function(id) {
+
+        };
     }
 })();
