405df4a23edcfd116728418976556463bf54701c
Júlio L. Jr <julio.leitao.jr@gmail.com>
2016-05-12 01:57:56 -0300

    correcao do job

--- module/job.js
+++ module/job.js
@@ -12,15 +12,17 @@
         cursor.exec(function(error, data) {
             var agora = new Date();
 
-            var cursorStudent = Student.findById(data[index].student);
+   
+
+            for (var index in data) {
+                var inicio = new Date(data[index].start);
+
+                         var cursorStudent = Student.findById(data[index].student);
                 cursorStudent.exec(function (error, student) {
                     if (student.pushToken) {
                         notificacao.sendPushNotification(student.pushToken, "Atenção você está próximo de ter uma aula " + data[index].type);
                     }
                 });
-
-            for (var index in data) {
-                var inicio = new Date(data[index].start);
 
                 if (agora.getHours() === inicio.getHours() 
                     && agora.getDay() === inicio.getDay() 
